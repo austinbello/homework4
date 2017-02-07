f6ad0cb0bba90d378acacc393ae59e3f6e1ced46
Austin Bello <austin@automatedinsights.com>
2017-02-07 11:42:43 -0500

    validation array

--- contact_validator.rb
+++ contact_validator.rb
@@ -6,12 +6,18 @@ class ContactValidator
     @data = CSV.read(filename)
   end
 
-  def get_numbers
-    data.flatten.select {|s| s.match(/\d{3}-\d{3}-\d{4}/)}
-  end
-
   def true_false_array(column, regex)
     column_num = data[0].index(column)
     data.drop(1).map {|row| row[column_num].match(regex) != nil}
+  end
+
+  def validation_array
+    checking_array = []
+
+    checking_array << true_false_array("joined", /^([1][0-2]|[0]\d|\d)[\/-]([3][01]|[0-2]\d|\d)[\/-](\d{2}|\d{4})$/)
+    checking_array << true_false_array("email", /^([\w\d_\-\.]+)@([\w\d_\-\.]+)\.([\w]{2,5})$/)
+    checking_array << true_false_array("phone", /(^(\(([\d]{3})\)\s?|[^0]\d{2}(-|\s|\.))\d{3}(-|\s|\.)\d{4}$)|(^[^0]\d{9}$)/)
+
+    checking_array
   end
 end
--- contact_validator_test.rb
+++ contact_validator_test.rb
@@ -7,15 +7,15 @@ class ContactValidatorTest < Minitest::Test
     assert ContactValidator.new('simple.csv')
   end
 
-  def test_simple
-    validator = ContactValidator.new('simple.csv')
-    assert_equal ['919-111-1111'], validator.get_numbers
-  end
-
   def test_true_false_array
     validator = ContactValidator.new('simple.csv')
     assert_equal [true, false], validator.true_false_array("joined", /^([1][0-2]|[0]\d|\d)[\/-]([3][01]|[0-2]\d|\d)[\/-](\d{2}|\d{4})$/)
-    assert_equal [true, true], validator.true_false_array("phone", /(^(\(([\d]{3})\)\s?|[^0]\d{2}(-|\s|\.))\d{3}(-|\s|\.)\d{4}$)|(^[^0]\d{9}$)/)
     assert_equal [true, false], validator.true_false_array("email", /^([\w\d_\-\.]+)@([\w\d_\-\.]+)\.([\w]{2,5})$/)
+    assert_equal [true, true], validator.true_false_array("phone", /(^(\(([\d]{3})\)\s?|[^0]\d{2}(-|\s|\.))\d{3}(-|\s|\.)\d{4}$)|(^[^0]\d{9}$)/)
+  end
+
+  def test_validation_array
+    validator = ContactValidator.new('simple.csv')
+    assert_equal [[true, false], [true, false], [true, true]], validator.validation_array
   end
 end
